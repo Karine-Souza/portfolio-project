b7a22f286a59469fad4ade4f7113a9f27dfae2e2
Karine Souza <karine.souza@valtech.com>
2022-12-23 11:01:40 -0300

    Image component

---
+++ components/atoms/P_Image.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="image">
+    <img
+      :src="src"
+      :alt="alt"
+      :class="aditionalClass"
+      :href="href"
+      :width="width"
+      :height="height"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+    },
+    alt: {
+      type: String,
+    },
+    aditionalClass: {
+      type: String,
+    },
+    href: {
+      type: String,
+    },
+    width: {
+      type: String,
+    },
+    height: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.image {
+  margin: 0;
+  padding: 0;
+}
+</style>
