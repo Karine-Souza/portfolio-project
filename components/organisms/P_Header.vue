a298673b9fc8ea2c620205e823534fbd2e17394b
Karine Souza <karine.souza@valtech.com>
2022-12-23 10:23:50 -0300

    Header component

---
+++ components/organisms/P_Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <P_Menu />
+    <P_Hero_image />
+  </div>
+</template>
+
+<script>
+import P_Menu from "../organisms/P_Menu.vue";
+import P_Hero_image from "../molecules/P_Hero_image.vue";
+
+export default {
+  components: {
+    P_Menu,
+    P_Hero_image,
+  },
+};
+</script>
+
+<style lang="scss"></style>
