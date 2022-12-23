baf413f4dc47d6ede016b4fbad2be158371c2dcf
Karine Souza <karine.souza@valtech.com>
2022-12-23 10:28:10 -0300

    Banner component

---
+++ components/molecules/P_Banner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="banner">
+    <P_Hero_image />
+  </div>
+</template>
+
+<script>
+import P_Hero_image from "../molecules/P_Hero_image.vue";
+
+export default {
+  components: {
+    P_Hero_image,
+  },
+};
+</script>
+
+<style lang="scss"></style>
