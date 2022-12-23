36c0dda8cc230aacec2ac4a5ef000139efe0ef0a
Karine Souza <karine.souza@valtech.com>
2022-12-23 10:33:32 -0300

    Hero image component

---
+++ components/molecules/P_Hero_image.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="heroImage">
+    <div
+      class="heroImage-container"
+      :style="{
+        backgroundColor: '#EDF3FF',
+      }"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style lang="scss">
+.heroImage {
+  width: 100%;
+  height: 880px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 73px 0 0;
+  .heroImage__container {
+    display: flex;
+    flex-direction: row-reverse;
+    padding: 60px;
+  }
+}
+</style>
