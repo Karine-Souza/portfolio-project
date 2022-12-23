bcce9e52addb3813659c572c24a5a02e6f8ba960
Karine Souza <karine.souza@valtech.com>
2022-12-23 10:38:54 -0300

    Testimonials component

---
+++ components/organisms/P_Testimonials.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="testimonials"></div>
+</template>
+
+<script>
+import P_Card_testimonials from "../molecules/P_Card_testimonials.vue";
+
+export default {
+  components: {
+    P_Card_testimonials,
+  },
+};
+</script>
+
+<style lang="scss"></style>
