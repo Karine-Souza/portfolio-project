01305cc1a4c30016873a04e421280906791c7d9b
Karine Souza <karine.souza@valtech.com>
2023-01-15 22:58:56 -0300

    banner done

--- components/molecules/P_Banner.vue
+++ components/molecules/P_Banner.vue
@@ -1,17 +1,43 @@
 <template>
   <div class="banner">
-    <P_Hero_image />
+    <P_Hero_image
+      class="banner__text"
+      :link="'ABOUT'"
+      :titulo="'An Experience Webflow Expert'"
+      :subtitulo="'Provides a full service range'"
+      :texto="`Ability to put themselves in the merchant's shoes. It is meant to partner on the long run, and work as an extension of the merchant's team.`"
+      :backgroundColor="'#28293E'"
+      :src="require('@/assets/images/pic (5).png')"
+    />
   </div>
 </template>
 
 <script>
 import P_Hero_image from "../molecules/P_Hero_image.vue";
+import P_Button from "../atoms/P_Button.vue";
 
 export default {
   components: {
     P_Hero_image,
+    P_Button,
   },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.banner {
+  h1 {
+    color: #ffffff;
+  }
+
+  h5 {
+    color: #ffffff;
+  }
+
+  p {
+    color: rgba(255, 255, 255, 0.64);
+  }
+  .heroImage__container {
+  }
+}
+</style>
