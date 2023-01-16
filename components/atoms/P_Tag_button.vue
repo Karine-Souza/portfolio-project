31106c4b4a01f0cd93155434c239ec4a76de884d
Karine Souza <karine.souza@valtech.com>
2023-01-15 23:48:50 -0300

    Tag butão adicionado

---
+++ components/atoms/P_Tag_button.vue
@@ -0,0 +1,52 @@
+<template>
+  <a
+    class="tagButton"
+    :class="aditionalClasses"
+    :href="href"
+    target="_blank"
+    @click="func"
+    :style="{ background: backgroundColor }"
+    >{{ label }}</a
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    aditionalClasses: {
+      type: String,
+    },
+    func: {
+      type: Function,
+      default: () => {},
+    },
+    backgroundColor: {
+      type: String,
+      default: "#FFFFFF",
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.tagButton {
+  position: relative;
+  display: flex;
+  width: 102px;
+  height: 38px;
+  border-radius: 19px;
+  color: #391400;
+  font: normal normal 400 16px/32px "Epilogue";
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+</style>
