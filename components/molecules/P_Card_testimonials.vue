4df4f10a6137bd9123dde2eea16b1ba0d5fc89a2
Karine Souza <karine.souza@valtech.com>
2023-01-17 11:18:09 -0300

    Card-testimonials WIP

--- components/molecules/P_Card_testimonials.vue
+++ components/molecules/P_Card_testimonials.vue
@@ -1,11 +1,53 @@
 <template>
-  <div class="cardTestimonials"></div>
+  <div class="cardTestimonials" :style="{ background: backgroundColor }">
+    <div class="cardTestimonials__content">
+      <img :src="srcStars" :alt="altStars" class="cardTestimonials__Stars" />
+      <p class="cardTestimonials__Testimonials">{{ testimonials }}</p>
+    </div>
+    <div class="cardTestimonials__autor">
+      <img
+        :src="srcAutorImg"
+        :alt="altAutorImg"
+        class="cardTestimonials__Img"
+      />
+      <h5 class="cardTestimonials__nome">{{ nome }}</h5>
+      <p class="cardTestimonials__profissão">{{ profissão }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   components: {},
+  props: {
+    backgroundColor: {
+      type: String,
+      default: "#FFFFFF",
+    },
+    srcStars: {
+      type: String,
+    },
+    testimonials: {
+      type: String,
+    },
+    srcAutorImg: {
+      type: String,
+    },
+    nome: {
+      type: String,
+    },
+    profissão: {
+      type: String,
+    },
+  },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.cardTestimonials {
+  position: absolute;
+  width: 586px;
+  height: auto;
+  border: 1px solid #f3d1bf;
+}
+</style>
