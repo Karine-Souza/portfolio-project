17cc45fe13bc60653925ec02989e86a6ff8c5a00
Karine Souza <karine.souza@valtech.com>
2023-01-18 18:53:44 -0300

    Mudanças nas classes do menu

--- components/molecules/P_Menu.vue
+++ components/molecules/P_Menu.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="menu">
-    <div class="menu__content">
-      <div class="menu_logo">
-        <img src="../../assets/images/logo.png" class="footer_logoImg" />
-      </div>
-      <div class="menu__links">
-        <ul class="menu__list">
-          <li class="menu__listItems">
-            <a class="menu__item menu__item1" href="#" target="_blank">About</a>
-          </li>
-          <li>
-            <a class="menu__item menu__item2" href="#" target="_blank"
-              >Services</a
-            >
-          </li>
-          <li>
-            <a class="menu__item menu__item3" href="#" target="_blank"
-              >Pricing</a
-            >
-          </li>
-          <li>
-            <a class="menu__item menu__item3" href="#" target="_blank">Blog</a>
-          </li>
-        </ul>
-      </div>
+    <div class="menu_logo">
+      <img src="../../assets/images/logo.png" class="footer_logoImg" />
+    </div>
+    <div class="menu__links">
+      <ul class="menu__list">
+        <li class="menu__listItems">
+          <a class="menu__item menu__item1" href="#" target="_blank">About</a>
+        </li>
+        <li>
+          <a class="menu__item menu__item2" href="#" target="_blank"
+            >Services</a
+          >
+        </li>
+        <li>
+          <a class="menu__item menu__item3" href="#" target="_blank">Pricing</a>
+        </li>
+        <li>
+          <a class="menu__item menu__item3" href="#" target="_blank">Blog</a>
+        </li>
+      </ul>
       <P_Button
         class="menu__button"
         label="Contact"
@@ -46,23 +42,17 @@ export default {
 <style lang="scss">
 .menu {
   display: flex;
+  background-color: #28293e;
+  color: #ffffff;
   justify-content: space-between;
   align-items: center;
-  &__content {
-    width: 100%;
-    display: flex;
-    background-color: #28293e;
-    color: #ffffff;
-    justify-content: space-between;
-    align-items: center;
-    padding: 14px 35px;
+  padding: 14px 35px;
 
-    @media (min-width: 992px) {
-      padding: 14px 35px;
-    }
-    @media (min-width: 1280px) {
-      padding: 14px 135px;
-    }
+  @media (min-width: 992px) {
+    padding: 14px 35px;
+  }
+  @media (min-width: 1280px) {
+    padding: 14px 135px;
   }
 
   &__button {
@@ -83,8 +73,9 @@ export default {
       display: none;
     }
     @media (min-width: 1280px) {
-      width: 700px;
-      display: block;
+      width: 75%;
+      justify-content: space-between;
+      display: flex;
     }
   }
 
@@ -93,6 +84,7 @@ export default {
     list-style: none;
     justify-content: space-evenly;
     padding: 0;
+    width: 80%;
   }
 
   &__item {
